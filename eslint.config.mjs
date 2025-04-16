711a3872da1c54ebebc071cbd8feef6e328b0643
Kent <dylankentish@live.co.uk>
2025-04-16 17:59:48 +0100

    correct eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
 import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
-import eslintPluginJest from 'eslint-plugin-jest/recommended';
+import eslintPluginJest from 'eslint-plugin-jest';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -14,7 +14,20 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   eslintPluginPrettier,
-  eslintPluginJest,
+  {
+    files: ['**/*.test.js'],
+    plugins: { jest: eslintPluginJest },
+    languageOptions: {
+      globals: eslintPluginJest.environments.globals.globals,
+    },
+    rules: {
+      'jest/no-disabled-tests': 'warn',
+      'jest/no-focused-tests': 'error',
+      'jest/no-identical-title': 'error',
+      'jest/prefer-to-have-length': 'warn',
+      'jest/valid-expect': 'error',
+    },
+  },
   {
     ignores: ['src/graph/*'],
   },
