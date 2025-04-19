2749df16d2951bb734f52f5e16e959aae492ad3b
Kent <dylankentish@live.co.uk>
2025-04-19 19:07:47 +0100

    update footer

--- src/components/core/footer.tsx
+++ src/components/core/footer.tsx
@@ -4,8 +4,8 @@ import Link from 'next/link';
 export const Footer: React.FC = () => {
   return (
     <footer className="border-border bg-background/70 w-full border-t">
-      <div className="container mx-auto my-12 flex flex-col items-center justify-between gap-10 px-10 sm:flex-row sm:items-start">
-        <div className="flex flex-col items-start gap-5">
+      <div className="container mx-auto my-12 flex flex-col items-center justify-between gap-10 px-10 sm:flex-row">
+        <div className="flex flex-col gap-2">
           <Link href="/" className="flex items-center gap-1 text-xl font-bold uppercase">
             <Image
               src="/icon.png"
@@ -16,60 +16,67 @@ export const Footer: React.FC = () => {
             />
             HASHCRASH
           </Link>
-          <div className="flex items-center justify-start gap-2">
+          <span className="text-muted-foreground text-xs">
+            made with ❤ by{' '}
             <Link
-              href="https://github.com/"
-              className="transition-opacity duration-300 hover:opacity-60 motion-reduce:transition-none"
-              rel="noopener noreferrer"
-              target="_blank"
-              aria-label="GitHub"
-            >
-              <Image
-                src="https://cdn.simpleicons.org/github/black/white"
-                className="size-3 stroke-current md:size-4"
-                alt="GitHub"
-                width={24}
-                height={24}
-                unoptimized
-              />
-            </Link>
-            <Link
-              href="https://x.com/GrindTheCoin"
-              className="transition-opacity duration-300 hover:opacity-60 motion-reduce:transition-none"
+              href="https://x.com/BuiltLabs"
+              className="hover:underline"
               rel="noopener noreferrer"
               target="_blank"
               aria-label="Twitter"
             >
-              <Image
-                src="https://cdn.simpleicons.org/x/black/white"
-                className="size-3 stroke-current md:size-4"
-                alt="Twitter"
-                width={24}
-                height={24}
-                unoptimized
-              />
+              Built Labs
             </Link>
-            <Link
-              href="https://grind.bearish.af/"
-              download
-              className="transition-opacity duration-300 hover:opacity-60 motion-reduce:transition-none"
-              rel="noopener noreferrer"
-              target="_blank"
-              aria-label="$GRIND VIBECODE HACKATHON"
-            >
-              $Grind
-            </Link>
-          </div>
+          </span>
         </div>
-        <div className="flex items-end gap-5 whitespace-nowrap sm:flex-col">
+        <div className="flex items-center justify-center gap-2">
           <Link
             href="https://x.com/BuiltLabs"
             className="transition-opacity duration-300 hover:opacity-60 motion-reduce:transition-none"
             rel="noopener noreferrer"
             target="_blank"
-            aria-label="Built Labs Twitter"
+            aria-label="Twitter"
           >
-            BUILT LABS
+            <Image
+              src="https://cdn.simpleicons.org/x/black/white"
+              className="size-4 stroke-current"
+              alt="Twitter"
+              width={24}
+              height={24}
+              unoptimized
+            />
+          </Link>
+          <Link
+            href="https://github.com/builtlabs/grind-hackathon"
+            className="transition-opacity duration-300 hover:opacity-60 motion-reduce:transition-none"
+            rel="noopener noreferrer"
+            target="_blank"
+            aria-label="Website GitHub"
+          >
+            <Image
+              src="https://cdn.simpleicons.org/github/black/white"
+              className="size-4 stroke-current"
+              alt="GitHub"
+              width={24}
+              height={24}
+              unoptimized
+            />
+          </Link>
+          <Link
+            href="https://github.com/builtlabs/grind-hackathon-contracts"
+            className="transition-opacity duration-300 hover:opacity-60 motion-reduce:transition-none"
+            rel="noopener noreferrer"
+            target="_blank"
+            aria-label="Solidity Contracts GitHub"
+          >
+            <Image
+              src="https://cdn.simpleicons.org/solidity/black/white"
+              className="size-4 stroke-current"
+              alt="GitHub"
+              width={24}
+              height={24}
+              unoptimized
+            />
           </Link>
         </div>
       </div>
